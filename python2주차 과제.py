419953daa5af6f2c4bd4c78c69d206cdc8a53b5b
kyunghyunlee311 <311lkh@gmail.com>
2021-05-04 16:08:29 +0900

    2주차 과제

---
+++ python2주차 과제.py
@@ -0,0 +1,9 @@
+#2주차 과제
+a = input("이름을 입력해주세요")
+print ("이름:", a)
+b = input("파트를 입력해주세요")
+print ("파트 :", b)
+c = input("어느 기수 입니까?")
+print ("기수 :", c)
+d = input("OB입니까 YB입니까")
+print("저는 :", d, "입니다.")
